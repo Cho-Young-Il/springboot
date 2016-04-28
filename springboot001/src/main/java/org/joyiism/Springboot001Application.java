f57cfccd01ff2ce4a3543c3e754b0a01316e2f37
Macintosh HD <joyikr@gmail.com>
2016-04-28 15:05:32 +0900

    create project

---
+++ springboot001/src/main/java/org/joyiism/Springboot001Application.java
@@ -0,0 +1,12 @@
+package org.joyiism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot001Application.class, args);
+	}
+}
---
+++ springboot001/src/test/java/org/joyiism/Springboot001ApplicationTests.java
@@ -0,0 +1,18 @@
+package org.joyiism;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Springboot001Application.class)
+@WebAppConfiguration
+public class Springboot001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
