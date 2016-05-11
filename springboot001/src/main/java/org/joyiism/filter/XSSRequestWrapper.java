9cfce6451d946a249f566824d3299931fc086b03
Macintosh HD <joyikr@gmail.com>
2016-05-11 12:52:39 +0900

    java xssfilter

---
+++ springboot001/src/main/java/org/joyiism/filter/XSSRequestWrapper.java
@@ -0,0 +1,74 @@
+package org.joyiism.filter;
+
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class XSSRequestWrapper extends HttpServletRequestWrapper {
+	private static Pattern[] patterns = new Pattern[] {
+			Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE),
+			Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL),
+			Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL),
+			Pattern.compile("</script>", Pattern.CASE_INSENSITIVE),
+			Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE
+					| Pattern.MULTILINE | Pattern.DOTALL),
+			Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE
+					| Pattern.MULTILINE | Pattern.DOTALL),
+			Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE
+					| Pattern.MULTILINE | Pattern.DOTALL),
+			Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE),
+			Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE),
+			Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE
+					| Pattern.MULTILINE | Pattern.DOTALL)
+	};
+
+	public XSSRequestWrapper(HttpServletRequest servletRequest) {
+		super(servletRequest);
+	}
+
+	@Override
+	public String[] getParameterValues(String parameter) {
+		String[] values = super.getParameterValues(parameter);
+		if (values == null) {
+			return null;
+		}
+		int count = values.length;
+		String[] encodedValues = new String[count];
+		for (int i = 0; i < count; i++) {
+			encodedValues[i] = stripXSS(values[i]);
+		}
+		return encodedValues;
+
+	}
+
+	@Override
+	public String getParameter(String parameter) {
+		String value = super.getParameter(parameter);
+		return stripXSS(value);
+
+	}
+
+	@Override
+	public String getHeader(String name) {
+		String value = super.getHeader(name);
+		return stripXSS(value);
+
+	}
+
+	private String stripXSS(String value) {
+		if (value != null) {
+			value = value.replaceAll("\0", "");
+			for (Pattern scriptPattern : patterns) {
+				if ( scriptPattern.matcher(value).matches() ) {
+					value=value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");		
+				}
+			}
+		}
+		return value;
+	}
+}
