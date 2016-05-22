70e701ce41ea241f1134a58ab41c272c9aa321cc
Macintosh HD <joyikr@gmail.com>
2016-05-23 01:15:02 +0900

    none

--- springboot001/src/main/java/org/joyiism/filter/XSSFilter.java
+++ springboot001/src/main/java/org/joyiism/filter/XSSFilter.java
@@ -11,15 +11,11 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @WebFilter("/*")
 public class XSSFilter implements Filter {
-	private static final Logger logger = 
-			LoggerFactory.getLogger(XSSFilter.class);
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {}
 
@@ -27,7 +23,6 @@ public class XSSFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		chain.doFilter(new XSSRequestWrapper((HttpServletRequest) request), response);
-		logger.info("xss do filter");
 	}
 
 	@Override
